608892149f647afbf527b4b2634937ca55a5c403
micjaw310589 <mj310589@student.polsl.pl>
2025-05-17 23:26:57 +0200

    raw Identity implementation

--- restauracja/restauracja/Program.cs
+++ restauracja/restauracja/Program.cs
@@ -1,12 +1,13 @@
 using restauracja.Components;
 using restauracja.Data;
-using restauracja.Models;
 //using Pomelo.EntityFrameworkCore.MySql;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-//using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using restauracja.Components.Account;
+using restauracja.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,13 +21,29 @@ builder.Services.AddDbContext<RestauracjaContext>(options =>
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
     ));
 
-var app = builder.Build();
+builder.Services.AddCascadingAuthenticationState();
 
-builder.Services.AddIdentity<User, Role>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-})
-    .AddEntityFrameworkStores<RestauracjaContext>();
+builder.Services.AddScoped<IdentityUserAccessor>();
+
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+builder.Services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RestauracjaContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<User>, IdentityNoOpEmailSender>();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -38,16 +55,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapAdditionalIdentityEndpoints();;
 
 app.Run();
